13319e0922669c003e17acdf43d75246dfeae77d
dynann <dynannran@gmail.com>
2024-11-06 23:14:08 +0700

    I am done with tailwind

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -4,6 +4,11 @@ class TasksController < ApplicationController
   # GET /tasks or /tasks.json
   def index
     @tasks = Task.all
+    if params[:sort] == 'desc'
+      @tasks = Task.order(status: :desc)
+    else 
+      @tasks = Task.order(:status)
+    end
   end
 
   # GET /tasks/1 or /tasks/1.json
